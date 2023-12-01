1306757f34b3670e3917c9f3d5b618789feb1470
hanjoon <45251507+wkdehf217@users.noreply.github.com>
2023-12-01 10:35:24 +0900

    Update 할인 행사.java

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,6 +2,9 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        
+        // 원하는 제품 수 만큼 리스트 생성
+        // banana 3개, apple 2개 이런식으로
         List<String> wanted = new ArrayList<>();
         for (int i = 0; i < number.length; i++) {
             for (int j = 1; j < number[i] + 1; j++) {
@@ -9,6 +12,14 @@ class Solution {
             }
         }
 
+        // 할인 일 수 -9 만큼 반복하면서
+        // 플래그를 두고
+        // Linkedhashmap을 사용해서 정렬되는 맵을 선언함
+        // discount의 0~10번째 값을 Map에 넣고
+        // wanted의 값을 Map에 넣어주는데, 이때 같은것이 있다면 -1, 없다면 -1을 넣음
+        // = dismap에는 wanted의 pot이 없기 때문에 -1이 들어가게 됨.
+        // = dismap은 chicken = 1, banana = -1, apple = 1, rice = 0, pork = 0, pot = -2
+        // 고로 flag = false, 다음것으로 넘어감
         for (int i = 0; i < discount.length-9; i++) {
             Boolean flag = true;
             HashMap<String, Integer> disMap = new LinkedHashMap<String, Integer>();
