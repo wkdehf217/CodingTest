7f39180b779bcf05ebd639a8f5b27df50ea7cc12
hanjoon <45251507+wkdehf217@users.noreply.github.com>
2023-12-14 10:59:52 +0900

    Update 숫자 짝꿍.java

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -24,3 +24,50 @@ class Solution {
         }
     }
 }
+
+// import java.lang.StringBuilder;
+// import java.util.*;
+// class Solution {
+//     public String solution(String X, String Y) {
+//         String answer = "";
+        
+//         Integer[] xToInt = new Integer[X.length()];
+//         Integer[] yToInt = new Integer[Y.length()];
+
+//         String[] xSplit = X.split("");
+//         String[] ySplit = Y.split("");
+
+//         for (int i = 0; i < X.length(); i++) {
+//             xToInt[i] = Integer.parseInt(xSplit[i]);
+//         }
+
+//         for (int i = 0; i < Y.length(); i++) {
+//             yToInt[i] = Integer.parseInt(ySplit[i]);
+//         }
+        
+//         for (int i = 0; i < X.length(); i++) {
+//             for (int j = 0; j < Y.length(); j++) {
+//                 if (Objects.equals(xToInt[i], yToInt[j]) && yToInt[j] != -1) {
+//                     answer += String.valueOf(xToInt[i]);
+//                     yToInt[j] = -1;
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         String answer2 = "";
+//         if(answer.isEmpty()) answer2 = "-1";
+//         else if(Integer.valueOf(answer) == 0) answer2 = "0";
+//         else{
+//             String[] splitAnswer = answer.split("");
+//             Arrays.sort(splitAnswer,Collections.reverseOrder());
+
+//             for (String s : splitAnswer) {
+//                 answer2 += s;
+//             }
+//         }
+        
+        
+//         return answer2;
+//     }
+//}
