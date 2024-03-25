b371839cae7c6579782dac562da11c4d7fdde6b4
hanjoon <45251507+wkdehf217@users.noreply.github.com>
2024-03-25 17:00:41 +0900

    [level 1] Title: 모의고사, Time: 0.74 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        
+        int[] person1 = {1, 2, 3, 4, 5};
+        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        int[] cnt = new int[3];
+        
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == person1[i%5]) cnt[0]++;
+            if(answers[i] == person2[i%8]) cnt[1]++;
+            if(answers[i] == person3[i%10]) cnt[2]++;
+        }
+        
+        int max = Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
+        System.out.println(max);
+        
+        List<Integer> list = new ArrayList<>();
+        
+        if(max == cnt[0])
+            list.add(1);
+        if(max == cnt[1])
+            list.add(2);
+        if(max == cnt[2])
+            list.add(3);
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+       
+        return answer;
+    }
+}
