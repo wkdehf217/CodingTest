0b39474a07419cdf42211263cc76366b4467ed8d
hanjoon <45251507+wkdehf217@users.noreply.github.com>
2024-04-23 15:23:57 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.77 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -3,12 +3,12 @@ class Solution {
         String answer = "";
         String[] split = s.split("");
         int cnt = 0;
+        
         for(String str : split){
-            
-         cnt = str.contains(" ") ? 0 : cnt + 1;
-            answer += cnt % 2 == 0 ? str.toLowerCase() : str.toUpperCase(); 
-    }
+            cnt = str.contains(" ") ?  0 : cnt + 1;
+            answer += cnt%2 == 0 ? str.toLowerCase() : str.toUpperCase(); 
+        }
+        
         return answer;
     }
 }
-
